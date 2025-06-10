42d92752f33dce2460bb631834565406bf36a1e9
jlbuild <juliabuildbot@gmail.com>
2025-06-10 17:17:23 +0000

    Qt6QuickTimeline_jll build 6.8.2+0

---
+++ src/Qt6QuickTimeline_jll.jl
@@ -0,0 +1,9 @@
+# Use baremodule to shave off a few KB from the serialized `.ji` file
+baremodule Qt6QuickTimeline_jll
+using Base
+using Base: UUID
+import JLLWrappers
+
+JLLWrappers.@generate_main_file_header("Qt6QuickTimeline")
+JLLWrappers.@generate_main_file("Qt6QuickTimeline", UUID("aaa24d55-1a42-52ef-a112-65a2da50b610"))
+end  # module Qt6QuickTimeline_jll
